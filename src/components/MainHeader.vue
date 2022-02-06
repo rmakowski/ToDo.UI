cda0518f8bc01cf6071c90e7ab2ba61df4962df1
Radoslaw Makowski <makowski.rado@gmail.com>
2022-02-06 14:54:11 +0100

    Customize style

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="header">
+		<h1>
+			<img alt="Check mark" src="../assets/check-mark.svg" class="svg" />
+			ToDo application
+			<img alt="Check mark" src="../assets/check-mark.svg" class="svg" />
+		</h1>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
